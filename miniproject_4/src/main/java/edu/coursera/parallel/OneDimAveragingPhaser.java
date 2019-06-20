0bbbfbc699e728cde924a0c425af120661e44245
Danio <daniowang20101@gmail.com>
2019-06-19 20:52:50 -0700

    project 4 implementation

--- miniproject_4/src/main/java/edu/coursera/parallel/OneDimAveragingPhaser.java
+++ miniproject_4/src/main/java/edu/coursera/parallel/OneDimAveragingPhaser.java
@@ -1,6 +1,7 @@
 package edu.coursera.parallel;
 
 import java.util.concurrent.Phaser;
+import java.io.*;
 
 /**
  * Wrapper class for implementing one-dimensional iterative averaging using
@@ -53,12 +54,12 @@ public final class OneDimAveragingPhaser {
             final int tasks) {
         Phaser ph = new Phaser(0);
         ph.bulkRegister(tasks);
+//        System.err.printf("array len %d, n val : %d\n", myNew.length, n);
 
         Thread[] threads = new Thread[tasks];
 
         for (int ii = 0; ii < tasks; ii++) {
             final int i = ii;
-
             threads[ii] = new Thread(() -> {
                 double[] threadPrivateMyVal = myVal;
                 double[] threadPrivateMyNew = myNew;
@@ -110,6 +111,41 @@ public final class OneDimAveragingPhaser {
     public static void runParallelFuzzyBarrier(final int iterations,
             final double[] myNew, final double[] myVal, final int n,
             final int tasks) {
+        Phaser [] phs = new Phaser[tasks];
+        for (int i = 0; i < tasks; i++) phs[i] = new Phaser(1);
+        Thread [] thr = new Thread[tasks];
+        final int chunk_size = (n + tasks - 1) / tasks;
+        for (int ii = 0; ii < tasks; ii++) {
+            final int i = ii;
+            thr[i] = new Thread(() -> {
+                double [] my_val_t = myVal;
+                double [] my_new_t = myNew;
+                final int s_idx = chunk_size * i + 1;
+                final int e_idx = Math.min(s_idx + chunk_size - 1, n);
+                for (int iter_cnt = 0; iter_cnt < iterations; iter_cnt++) {
+                    my_new_t[s_idx] = (my_val_t[s_idx - 1] + my_val_t[s_idx + 1]) / 2.0;
+                    my_new_t[e_idx] = (my_val_t[e_idx - 1] + my_val_t[e_idx + 1]) / 2.0;
+                    phs[i].arrive();
+                    for (int idx = s_idx + 1; idx <= e_idx - 1; idx++) {
+                        my_new_t[idx] = (my_val_t[idx - 1] + my_val_t[idx + 1]) / 2.0;
+                    }
+                    if (i > 0) phs[i - 1].awaitAdvance(iter_cnt);
+                    if (i < tasks - 1) phs[i + 1].awaitAdvance(iter_cnt);
+                    //perform swapping
+                    double [] temp_val = my_val_t;
+                    my_val_t = my_new_t;
+                    my_new_t = temp_val;
+                }
 
+            });
+            thr[i].start();
+        }
+        for (int ii = 0; ii < tasks; ii++) {
+            try {
+                thr[ii].join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
